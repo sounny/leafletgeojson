4cc8658b136e2f8044cfdab6617001896f59944e
sounny <sounny@gmail.com>
2024-02-06 19:41:39 -0500

    fixing file paths. commenting

--- AJAX.js
+++ AJAX.js
@@ -1,10 +1,10 @@
-function jsAjax(){
-    fetch('http://127.0.0.1:5500/data/MegaCities.geojson')
-        .then(callback) 
+function jsAjax(){ //This function uses the Fetch API to request the 'MegaCities.geojson' file.
+    fetch('MegaCities.geojson') //This line fetches the 'MegaCities.geojson' file.
+        .then(callback) //This line calls the callback function.
 };
 
-function callback(response){
-    console.log(response)
+function callback(response){ //This function logs the response to the console.
+    console.log(response) //This line logs the response to the console.
 }
 
-window.onload = jsAjax();
+window.onload = jsAjax(); // This line sets the jsAjax function to run when the window finishes loading.
